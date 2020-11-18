9d32dbc9b9ea2622e492c121282ba959d557b8cd
hauntedpavement <hauntedpavement@google.com>
2020-11-17 18:24:58 -0600

    added popover programmatically

---
+++ src/components/FilterPopover.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-content>
+    <ion-list>
+      <ion-item>item 1</ion-item>
+      <ion-item>item 2</ion-item>
+      <ion-item>item 3</ion-item>
+    </ion-list>
+  </ion-content>
+</template>
+
+<script lang="ts">
+import { IonContent, IonList, IonItem } from "@ionic/vue";
+import { defineComponent, onMounted } from "vue";
+export default defineComponent({
+  name: "FilterPopover",
+  components: {
+    IonContent,
+    IonList,
+    IonItem,
+  },
+  setup(props: any) {
+    const showProps = () => {
+      console.log(props);
+    };
+    onMounted( () => showProps());
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,11 @@
     <ion-header>
       <ion-toolbar color="primary">
         <ion-title>vue3gmaps</ion-title>
+        <ion-buttons slot="end">
+          <ion-button @click="presentPopover($event)">
+            <ion-icon :icon="filter"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
@@ -19,12 +24,18 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonIcon,
+  IonButton,
+  IonButtons,
 } from "@ionic/vue";
+import { filter } from "ionicons/icons";
 import { defineComponent, onMounted } from "vue";
 import { Plugins, GeolocationOptions } from "@capacitor/core";
+import FilterPopover from "@/components/FilterPopover.vue";
+import { popoverController } from "@ionic/core";
 
 declare const google: any;
-const { Geolocation } = Plugins; //capacitor object destructuring can b used since variable is same as API name. this will only use whatever plugins we need 
+const { Geolocation } = Plugins; //capacitor object destructuring can b used since variable is same as API name. this will only use whatever plugins we need
 
 export default defineComponent({
   name: "Home",
@@ -34,43 +45,60 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonToolbar,
+    // FilterPopover,
+    IonIcon,
+    IonButton,
+    IonButtons,
   },
-  
-  setup(){
+
+  setup() {
     let map: google.maps.Map;
     let centerMarker: google.maps.Marker;
     let initLocation: google.maps.LatLng;
-    const zoom = 13;  
-    const geolocationOpts: GeolocationOptions = { 
-      enableHighAccuracy: true
-    }
+    const zoom = 13;
+    const geolocationOpts: GeolocationOptions = {
+      enableHighAccuracy: true,
+    };
     const getCurrentLocation = async () => {
       const pos = await Geolocation.getCurrentPosition(geolocationOpts);
       const { latitude, longitude } = pos.coords;
       initLocation = new google.maps.LatLng(latitude, longitude);
-    }
+    };
+    const presentPopover = async (ev: Event) => {
+      const popover = await popoverController.create({
+        component: FilterPopover,
+        componentProps: {
+          value1: 4,
+          value2: "red meat",
+        },
+        animated: true,
+        translucent: true,
+        event: ev,
+        showBackdrop: false,
+        backdropDismiss: false,
+        cssClass: "custom-popover",
+      });
+      await popover.present();
+    };
     const initMap = async () => {
       await getCurrentLocation();
-      map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+      map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: initLocation,
         zoom: zoom,
-        clickableIcons: false
-      } )
+        clickableIcons: false,
+      });
       centerMarker = new google.maps.Marker({
         position: initLocation,
         map: map,
-      })
-    }
-    onMounted( () => initMap());
+      });
+    };
+    onMounted(() => initMap());
 
     return {
-      IonContent,
-      IonHeader,
-      IonPage,
-      IonTitle,
-      IonToolbar,
-    }
-  }
+      presentPopover,
+      filter,
+    };
+  },
 });
 </script>
 
